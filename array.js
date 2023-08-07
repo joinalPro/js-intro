a531225aa7080dbf902e633f866b31f4f10d3aa7
joinalzx <joinalzx@gmail.com>
2023-08-07 11:24:43 +0600

    array basic done

---
+++ array.js
@@ -0,0 +1,14 @@
+var arrayName = [45,21,215,21,4,65,89,87,84,98,99,]
+// console.log(arrayName[5])
+// var element = arrayName[1];
+// console.log(element);
+// arrayName[1] = 22;
+// console.log(arrayName)
+
+// var positionIndex = arrayName.indexOf(87);
+// console.log(positionIndex);
+
+arrayName.push(11);
+console.log(arrayName)
+arrayName.unshift(19);
+console.log(arrayName)
