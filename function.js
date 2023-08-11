07a4d5f06fc9d9e44d40eeb57c19c2e7a5bd523e
joinalzx <joinalzx@gmail.com>
2023-08-11 20:37:29 +0600

    function

--- function.js
+++ function.js
@@ -1,6 +1,27 @@
-function funcName(a, b, c, d) {
-    console.log(a, b, c, d );
-    var sum = a + b + c;
-    console.log(sum);
+// function funcName(a, b, c, d) {
+//     console.log(a, b, c, d );
+//     var sum = a + b + c;
+//     console.log(sum);
+// }
+// funcName(1, 2, 1, );
+
+//2
+// function bsingara(money){
+//     // console.log('singara den');
+//     console.log(money);
+// }
+// bsingara(12);
+
+//3
+// function bringSingara (money){
+//     console.log('taka disen ', money);
+//     console.log('ai nen singara');
+// }
+// var taka = 300;
+// bringSingara(taka);
+
+// 4
+function add (num1, mum2){
+    console.log('going to add ', num1, mum2);
 }
-funcName(1, 2, 1, );
+add(21);
