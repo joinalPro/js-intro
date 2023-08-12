7bd103e90f6395bf0561ede988627bf8c058f156
joinalzx <joinalzx@gmail.com>
2023-08-12 17:01:46 +0600

    object in loop

--- object-looping.js
+++ object-looping.js
@@ -4,5 +4,15 @@ var shoppingCart = {
     keyboard : 6,
     mouse: 1,
     pen: 22,
-    shoes 9
+    shoes: 9
+}
+const keys = Object.keys(shoppingCart);
+console.log(keys);
+const values = Object.values(shoppingCart);
+console.log(values);
+
+for (var i = 0; i < keys.length; i++){
+        var propertyName = keys[i];
+        var propertiesValue  = shoppingCart[propertyName];
+        console.log(propertyName, propertiesValue);
 }
